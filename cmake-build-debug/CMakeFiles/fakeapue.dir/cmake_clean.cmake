f7734ca4ede6f0531cef7cd205f7dfa90fd5b7c6
annsshadow <cravenboy@163.com>
2016-12-29 20:30:12 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/fakeapue.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fakeapue.dir/main.cpp.o"
+  "fakeapue.pdb"
+  "fakeapue"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fakeapue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/fakeapue.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for fakeapue.
+# This may be replaced when dependencies are built.
