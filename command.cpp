f90d6b75f06c17bf9bdaecf0165eadca73e84fd5
annsshadow <cravenboy@163.com>
2017-01-01 18:33:39 +0800

    command

---
+++ command.cpp
@@ -0,0 +1,44 @@
+//
+// Created by annsshadow on 2017/1/1.
+//
+
+#include "apue.h"
+#include <sys/wait.h>
+
+int main(void)
+{
+    char buf[MAXLINE];
+    pid_t pid;
+    int status;
+    int cur_buff_len;
+
+    printf("%% ");
+    while(fgets(buf,MAXLINE,stdin) != NULL)
+    {
+        cur_buff_len = strlen(buf);
+        if (buf[cur_buff_len - 1] == '\n')
+            buf[cur_buff_len - 1] = 0;
+
+        if ((pid = fork()) < 0)
+        {
+            //err_sys("fork error");
+            printf("fork error");
+        }
+        else if (pid == 0)
+        {
+            execlp(buf,buf,(char*)0);
+            //err_ret("can't execute:%s",buf);
+            printf("can't execute:%s",buf);
+            exit(127);
+        }
+
+        /*parent*/
+        if ((pid = waitpid(pid,&status,0)) < 0 )
+            //err_sys("waitpid error");
+            printf("waitpid error");
+
+        printf("%% ");
+    }
+
+    exit(0);
+}
